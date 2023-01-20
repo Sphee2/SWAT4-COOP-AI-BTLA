5d3664e815818d1d7444f29d733f36fd11a6f8d5
Sphee2 <81040419+Sphee2@users.noreply.github.com>
2023-01-20 05:57:10 +0400

    TSS System

---
+++ System/ConvertFonts.bat
@@ -0,0 +1,7 @@
+ucc execmacro koreanfonts.exec -nobind
+ucc execmacro simplechinesefonts.exec -nobind
+ucc execmacro traditionalchinesefonts.exec -nobind
+ucc execmacro germanfonts.exec -nobind
+ucc execmacro italianfonts.exec -nobind
+ucc execmacro spanishfonts.exec -nobind
+ucc execmacro frenchfonts.exec -nobind
---
+++ System/TestLocalizeSwat4.bat
@@ -0,0 +1,2 @@
+CreateLocalizationPackage
+ConvertAsianFonts
