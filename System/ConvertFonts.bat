13bf0e3fe2d2f602e29b2398c4dfb7b8f9157b8a
Sphee2 <81040419+Sphee2@users.noreply.github.com>
2023-01-20 06:17:12 +0400

    both systems

--- System/ConvertFonts.bat
+++
@@ -1,7 +0,0 @@
-ucc execmacro koreanfonts.exec -nobind
-ucc execmacro simplechinesefonts.exec -nobind
-ucc execmacro traditionalchinesefonts.exec -nobind
-ucc execmacro germanfonts.exec -nobind
-ucc execmacro italianfonts.exec -nobind
-ucc execmacro spanishfonts.exec -nobind
-ucc execmacro frenchfonts.exec -nobind
